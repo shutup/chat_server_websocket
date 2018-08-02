d3fe64644f44d36865a885203f6e4355490720a3
shutup <fyjc999@gmail.com>
2018-08-02 18:12:03 +0800

    init commit

---
+++ public/javascripts/mainNew.js
@@ -0,0 +1,104 @@
+var ws;
+$(function () {
+    var $messages = $('#messages');
+    var $m = $('#m');
+    var $chatForm = $('#chatForm');
+    var $signInForm = $('#signInForm');
+    var $username = $('#username');
+    var $signInArea = $('.signInArea');
+    var $chatArea = $('.chatArea');
+
+    ws = new WebSocket('ws://localhost:8080');
+    ws.onopen = function () {
+        console.log('websocket is connected ...');
+    };
+    ws.onmessage = function (ev) {
+        var cmd = JSON.parse(ev.data);
+        var action = cmd['action'];
+        var data = cmd['data'];
+        if (action == 'sign in ok') {
+            $signInArea.hide();
+            $chatArea.show();
+        } else if (action == 'user list') {
+            console.log(data);
+        } else if (action == 'new chat message') {
+            $messages.append(makeUpNewMessage(data));
+            $messages[0].scrollTop = $messages[0].scrollHeight;
+        }
+        console.log(ev);
+    };
+    ws.onclose = function (ev) {
+        console.log('Disconnected!');
+    };
+
+    $signInForm.submit(function () {
+        var username = $username.val().trim();
+        if (username.length > 0) {
+            var user = {
+                'action': 'sign in',
+                'data': username
+            }
+            ws.send(JSON.stringify(user));
+        }
+        return false;
+    });
+
+    $chatForm.submit(function () {
+        var data = {
+            'data': {}
+        };
+        var msg = $m.val();
+        if (msg.startsWith('/r ')) {
+            var parts = msg.split(' ');
+            if (parts.length >= 3) {
+                data.action = 'new chat message';
+                data.data.to = parts[1];
+                parts.shift();
+                parts.shift();
+                data.data.message = parts.join();
+            }
+        } else {
+            data.action = 'new group message';
+            data.data.to = 'room1';
+            data.data.message = msg;
+        }
+        ws.send(JSON.stringify(data));
+        $messages.append(makeUpMessage($m.val()));
+        $m.val('');
+        $messages[0].scrollTop = $messages[0].scrollHeight;
+        return false;
+    });
+
+    function makeUpNewMessage(data) {
+        var username = data.from;
+        var message = data.message;
+        var li = $('<li/>');
+
+        var avater = $('<div class="avatar"/>');
+        var avaterContent = $('<label/>');
+        avaterContent.text(username.toUpperCase().charAt(0));
+        avater.append(avaterContent);
+        var messageBubbleLeft = $('<div class="talk-bubble tri-right left-top"/>');
+        var messageContent = $('<div class="talktext"/>');
+        var p = $('<p/>');
+        p.text(username + ": " + message);
+        messageContent.append(p);
+        messageBubbleLeft.append(messageContent);
+        li.append(avater);
+        li.append(messageBubbleLeft);
+        return li;
+    };
+
+    function makeUpMessage(msg) {
+        var li = $('<li/>');
+        var messageBubbleRight = $('<div class="talk-bubble-right tri-right btm-right"/>');
+        var messageContent = $('<div class="talktext"/>');
+        var p = $('<p/>');
+        p.text(msg);
+        messageContent.append(p);
+        messageBubbleRight.append(messageContent);
+        li.append(messageBubbleRight);
+        return li;
+    }
+
+});
---
+++ service/chatroom.js
@@ -0,0 +1,88 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({port: 8080});
+var users = new Map();
+var user_sockets = new Map();
+var groups = {};
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        var cmd = JSON.parse(message);
+        var action = cmd['action'];
+        var data = cmd['data'];
+        if (action == 'sign in') {
+            ws.username = data.username;
+            user_sockets.set(data.username, ws);
+            //add to global list
+            users.set(data.username, data);
+
+            var result = {
+                'action': 'sign in ok'
+            };
+            ws.send(JSON.stringify(result));
+        }
+        else if (action == 'get user list') {
+            var user_list = Array.from(users.values());
+            var result = {
+                'action': 'user list',
+                'data': user_list
+            }
+            ws.send(JSON.stringify(result));
+        }
+        else if (action == 'create group') {
+
+        }
+        else if (action == 'new chat message') {
+            var client = user_sockets.get(data.to);
+            if (client !== undefined && client !== ws && client.readyState === WebSocket.OPEN) {
+                var result = {
+                    'action': 'new chat message',
+                    'data': {
+                        'from': ws.username,
+                        'to': data.to,
+                        'message': data.message
+                    }
+                };
+                client.send(JSON.stringify(result));
+            }
+        }
+        else if (action == 'new group message') {
+            var clients = groups.get(data.to);
+            clients.forEach(function each(client) {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    var result = {
+                        'action': 'new group message',
+                        'data': {
+                            'from': ws.username,
+                            'to': data.to,
+                            'message': data
+                        }
+                    };
+                    client.send(JSON.stringify(result));
+                }
+            })
+        } else if (action == 'ping') {
+            console.log('get ping at ' + new Date().getTime());
+            var result = {
+                'action': 'pong'
+            };
+            ws.send(JSON.stringify(result));
+        } else if (action == 'reconnect') {
+            console.log('client reconnect at ' + new Date().getTime());
+            ws.username = data.username;
+            user_sockets.set(data.username, ws);
+            //add to global list
+            users.set(data.username, data);
+        }
+    });
+
+    console.log('user connected');
+    console.log('current clients:' + wss.clients.size);
+
+    ws.on('close', function close() {
+        console.log('client disconnected');
+        users.delete(ws.username);
+        user_sockets.delete(ws.username);
+    });
+});
